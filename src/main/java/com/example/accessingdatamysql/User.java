a414177501baf13eeeff0373b3742c4b7498b2c1
Anand Tert <anand_tert@thbs.india.com>
2023-12-18 12:31:21 +0530

    Corrected the id and auto-generated in accordance with the database

--- src/main/java/com/example/accessingdatamysql/User.java
+++ src/main/java/com/example/accessingdatamysql/User.java
@@ -8,7 +8,7 @@ import jakarta.persistence.Id;
 @Entity // This tells Hibernate to make a table out of this class
 public class User {
   @Id
-  @GeneratedValue(strategy=GenerationType.AUTO)
+  @GeneratedValue(strategy=GenerationType.TABLE)
   private Integer id;
 
   private String name;
